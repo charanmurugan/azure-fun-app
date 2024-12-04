09efbe0281ec0a8ef823b83feb80a5a65299da4e
CharanM <murugancharan245@gmail.com>
2024-12-04 21:15:01 +0530

    deploy azure function app

---
+++ src/main/java/com/example/BlobTriggerFunction.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+
+public class BlobTriggerFunction {
+    @FunctionName("blobTriggerFunction")
+    public void run(
+        @BlobTrigger(name = "content", path = "cnb-poc/{name}", connection = "AzureWebJobsStorage")
+        String content,
+        @BindingName("name") String filename,
+        final ExecutionContext context
+    ) {
+        System.out.println("executed");
+        context.getLogger().info("Blob triggered! Name: " + filename + ", Content: " + content);
+        
+    }
+}
